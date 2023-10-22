91f62ebb0e6e638fe957175ef78458ca871cc82b
Kitae's Github <aqua0405@gmail.com>
2023-10-23 02:54:33 +0900

    웹팩 적용하기

--- README.md
+++ README.md
@@ -8,3 +8,6 @@ npx eslint --init
 npm i -D prettier
 
 npm i -D eslint-config-prettier eslint-plugin-prettier
+
+# 웹팩 설치 
+npm install --save-dev webpack webpack -cli webpack-dev-server typescript ts-loader
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 // 인스턴스 생성 - app.ts 파일이 실행될 때, 자동으로 실행된다.
-import { ProjectInput } from './components/project-input.js';
-import { ProjectList } from './components/project-list.js';
+import { ProjectInput } from './components/project-input';
+import { ProjectList } from './components/project-list';
 
 new ProjectInput();
 new ProjectList('active');
 new ProjectList('finished');
+
